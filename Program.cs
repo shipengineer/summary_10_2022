71a0b01044f48bd0e8c4a2f88fbc52762f169ffb
PoeticCrane <poeticcrane@gmail.com>
2022-10-11 21:30:44 +0300

    Wrote The programm

--- Program.cs
+++ Program.cs
@@ -12,6 +12,12 @@ string[] SearchForThrdCharString(string[] in_mas)
     int j = 0;
     for (int i = 0; i < in_mas.Length; i++)
     {
+        if (in_mas[i].Length <= 3)
+        {
+            Array.Resize(ref out_mas, 1 + j);
+            out_mas[j] = in_mas[i];
+            j++;
+        }
     }
     return out_mas;
 }
